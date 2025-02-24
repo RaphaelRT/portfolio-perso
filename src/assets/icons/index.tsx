8cf990843f9d63bccbfcd074a1fa980fcf66f6ba
Raphael Robert <raphaelrobert@Mac-mini-de-Raphael.local>
2025-02-24 20:33:10 +0100

    fix: bug svg [mobile]

--- src/assets/icons/index.tsx
+++ src/assets/icons/index.tsx
@@ -3,7 +3,7 @@ import settings from "@/app/settings";
 const { COLORS: { WHITE }} = settings
 const URL_FRIENDLY_WHITE = WHITE.replace("#", "%23")
 const Linkedin = () => (
-  <svg id="Calque_1" xmlns="http://www.w3.org/2000/svg" version="1.1" viewBox="0 0 225 175">
+  <svg id="Calque_1" xmlns="http://www.w3.org/2000/svg" version="1.1" viewBox="0 0 225 175" fill={ WHITE }>
     <rect x="37.5" y="75" width="25" height="75" fill={ URL_FRIENDLY_WHITE }/>
     <rect x="37.5" y="25" width="25" height="25" fill={ URL_FRIENDLY_WHITE }/>
     <rect x="87.5" y="75" width="25" height="75" fill={ URL_FRIENDLY_WHITE }/>
@@ -13,7 +13,7 @@ const Linkedin = () => (
 );
 
 const Mail = () => (
-  <svg viewBox="0 0 225 175">
+  <svg viewBox="0 0 225 175" fill={ WHITE }>
     <rect x="125" y="62.5" width="25" height="25" fill={ URL_FRIENDLY_WHITE }/>
     <rect x="200" y="37.5" width="25" height="100" fill={ URL_FRIENDLY_WHITE }/>
     <rect x="25" y="137.5" width="175" height="25" fill={ URL_FRIENDLY_WHITE }/>
@@ -24,7 +24,7 @@ const Mail = () => (
   </svg>
 )
 const Phone = () => (
-  <svg id="Calque_1" xmlns="http://www.w3.org/2000/svg" version="1.1" viewBox="0 0 225 175">
+  <svg id="Calque_1" xmlns="http://www.w3.org/2000/svg" version="1.1" viewBox="0 0 225 175" fill={ WHITE }>
     <rect x="87.5" y="112.5" width="25" height="25" fill={ URL_FRIENDLY_WHITE }/>
     <rect x="62.5" y="87.5" width="25" height="25" fill={ URL_FRIENDLY_WHITE }/>
     <polygon points="187.5 112.5 187.5 162.5 112.5 162.5 112.5 137.5 137.5 137.5 137.5 112.5 187.5 112.5" fill={ URL_FRIENDLY_WHITE }/>
@@ -33,7 +33,7 @@ const Phone = () => (
   </svg>
 )
 const Download = () => (
-  <svg id="Calque_1" xmlns="http://www.w3.org/2000/svg" version="1.1" viewBox="0 0 225 175">
+  <svg id="Calque_1" xmlns="http://www.w3.org/2000/svg" version="1.1" viewBox="0 0 225 175" fill={ WHITE }>
     <rect x="25" y="150.13" width="175" height="24.73" fill={ URL_FRIENDLY_WHITE }/>
     <rect x="50" y="50.13" width="25" height="25" fill={ URL_FRIENDLY_WHITE }/>
     <polygon points="150 74.96 150 99.86 125 99.86 125 125.13 100.05 125.13 100.05 100.13 75 100.13 75 75.13 100.05 75.13 100.05 .13 125 .13 125 74.96 150 74.96" fill={ URL_FRIENDLY_WHITE }/>
@@ -41,7 +41,7 @@ const Download = () => (
   </svg>
 )
 const Github = () => (
-  <svg id="Calque_1" xmlns="http://www.w3.org/2000/svg" version="1.1" viewBox="0 0 225 175">
+  <svg id="Calque_1" xmlns="http://www.w3.org/2000/svg" version="1.1" viewBox="0 0 225 175" fill={ WHITE }>
     <polygon points="175 0 175 50 75 50 75 0 100 0 100 24.83 150 24.83 150 0 175 0" fill={ URL_FRIENDLY_WHITE }/>
     <rect x="50" y="50" width="25" height="50" fill={ URL_FRIENDLY_WHITE }/>
     <rect x="25" y="125" width="25" height="25" fill={ URL_FRIENDLY_WHITE }/>
@@ -50,12 +50,12 @@ const Github = () => (
   </svg>
 )
 const BulletArrow = ({className}: { className?: string }) => (
-  <svg className={className} viewBox="0 0 11.7 19.51">
+  <svg className={className} viewBox="0 0 11.7 19.51" fill={ WHITE }>
     <path d="M3.9,3.9H0V0h3.9v3.9ZM7.8,7.8h-3.9v-3.9h3.9v3.9ZM11.7,11.7h-3.9v-3.9h3.9v3.9ZM7.8,15.6h-3.9v-3.9h3.9v3.9ZM3.9,19.51H0v-3.9h3.9v3.9Z" />
   </svg>
 )
 const Arrow = ({className}: { className?: string }) => (
-<svg className={className} id="Calque_1" xmlns="http://www.w3.org/2000/svg" version="1.1" viewBox="25 0 125 125">
+<svg className={className} id="Calque_1" xmlns="http://www.w3.org/2000/svg" version="1.1" viewBox="25 0 125 125" fill={ WHITE }>
   <rect x="24.73" y="49.74" width="25" height="25" fill={ URL_FRIENDLY_WHITE }/>
   <polygon points="124.73 74.57 124.73 99.47 99.73 99.47 99.73 124.74 74.78 124.74 74.78 99.74 49.73 99.74 49.73 74.74 74.78 74.74 74.78 -.26 99.73 -.26 99.73 74.57 124.73 74.57" fill={ URL_FRIENDLY_WHITE }/>
   <rect x="124.73" y="49.74" width="25" height="25" fill={ URL_FRIENDLY_WHITE }/>
